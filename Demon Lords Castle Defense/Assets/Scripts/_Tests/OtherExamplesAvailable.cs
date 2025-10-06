6eb7b01efb7ae5cd2dc2e21a86cfad159c82883c
Glenn Storm <Glenn.Storm@nyfa.edu>
2025-10-06 15:14:21 -0700

    glenn - commit for unity version 6000.0.58f LTS, other examples available added.

---
+++ Demon Lords Castle Defense/Assets/Scripts/_Tests/OtherExamplesAvailable.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class OtherExamplesAvailable : MonoBehaviour
+{
+    // Author: Glenn Storm
+    // This holds an assortment of comments on additional example scripts available
+
+    // I can provide examples in code for:
+
+    // - The Effects System (we used to great advantage in Greener Pastures; very flexible way to add arbitrary rules to anything)
+    // - Random Helper functions (shuffle, weighted random, gaussian random, etc.)
+    // - Save/Load functions (auto-load/save, manage directories, etc.)
+    // - Sound Capture
+    // - Screenshot Capture
+    // - Timer Helper functions (better than using coroutines; more useful everywhere)
+    // - Pattern of using Data, System and Tool scripts together in architecture
+
+}
