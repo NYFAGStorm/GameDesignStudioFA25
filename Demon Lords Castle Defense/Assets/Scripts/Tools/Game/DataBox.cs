5dc79553e29da91f77de4d6b32493151440151a2
Esther Li (YT) <yu.li2+gitHub@student.nyfa.edu>
2025-10-27 10:21:28 -0700

    written rules for the tooltip

---
+++ Demon Lords Castle Defense/Assets/Scripts/Tools/Game/DataBox.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class DataBox : MonoBehaviour
+{
+    // Written Rule (Esther)
+    // if clicking on an item (hero, tile, trophy, goon)
+    //  highlight said item
+    //  show tooltip on the top of said item
+    //      - if Hero, box follow hero
+    //      - if goon/trophy/tile, when item is dragged away, box disappear
+    //      - clicking away (on black space, on another item), box disappear
+    //  data shown (Hero):
+    //      - Name
+    //      - Current HP / Total HP
+    //      - Damage
+    //      - Attack on _____ beat
+    //      - Range: Melee or Range
+    //      - Attribute
+    //  data shown (Tile):
+    //      - Name
+    //      - Level
+    //      - Damage
+    //      - Attack on _____ beat
+    //      - Attribute
+    //      - Available slots
+    //  data shown (Goon):
+    //      - Name
+    //      - Current HP / Total HP
+    //      - Damage
+    //      - Attack on _____ beat
+    //      - Range: Melee or Range
+    //      - Attribute
+    //  data shown (Trohpy):
+    //      - Name
+    //      - Description
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
