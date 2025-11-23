f85753938e61ae3bed3f7fa76c259d9f5a556846
Esther Li (YT) <yu.li2@student.nyfa.edu>
2025-11-23 15:51:31 -0800

    WIP onboarding is reacting to clicks and display the next line, showing either tooltip or text box accordingly, non-talking character's sprite will dim. - textbox sprite, snowball sprite, demon lord sprite are temp, need replacing - need implementation of sprites with different emotions - clicking after reaching the end of tutorial isn't closing the tutorial

--- Demon Lords Castle Defense/Assets/Scripts/Systems/TutorialManager.cs
+++ Demon Lords Castle Defense/Assets/Scripts/Systems/TutorialManager.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour
 {
@@ -7,16 +9,76 @@ public class TutorialManager : MonoBehaviour
 
     private bool isSkipped = false;
 
-    public GameObject[] snowBallText;
+    public GameObject textbox;
+    public GameObject[] dialogueText;
+    public GameObject snowBall;
+    public GameObject demonLord;
     public Sprite[] snowBallSprites;
-    public GameObject[] demonLordText;
     public Sprite[] demonLordSprites;
 
-    public GameObject[] toolTipText;
+    private int i = 0;
 
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            
+            if (i < dialogueText.Length)
+            {
+                NextDialogue();
+            }
+
+            if (i >= dialogueText.Length)
+            {
+                this.gameObject.SetActive(false);
+                Debug.Log("End of Tutorial");
+            }
+            Debug.Log(i);
+        }
+    }
+
+    public void NextDialogue()
+    {
+            dialogueText[i].SetActive(false);
+            i++;
+            dialogueText[i].SetActive(true);
+
+            if (dialogueText[i].tag == "Snowball")
+            {
+                SnowballTalk();
+                demonLord.SetActive(true);
+                snowBall.SetActive(true);
+                snowBall.transform.Find("Snowball Name").gameObject.SetActive(true);
+                demonLord.transform.Find("Demon Lord Name").gameObject.SetActive(false);
+                textbox.GetComponent<Image>().enabled = true;
+            }
+            if (dialogueText[i].tag == "DemonLord")
+            {
+                DemonLordTalk();
+                demonLord.SetActive(true);
+                snowBall.SetActive(true);
+                snowBall.transform.Find("Snowball Name").gameObject.SetActive(false);
+                demonLord.transform.Find("Demon Lord Name").gameObject.SetActive(true);
+                textbox.GetComponent<Image>().enabled = true;
+            }
+            if (dialogueText[i].tag == "Tooltip")
+            {
+                textbox.GetComponent<Image>().enabled = false;
+                demonLord.SetActive(false);
+                snowBall.SetActive(false);
+            }        
+    }
+
+    private void SnowballTalk()
+    {
+        snowBall.GetComponent<Image>().color = new Color (1f, 1f, 1f);
+        demonLord.GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f);
+    }
+
+    private void DemonLordTalk()
+    {
+        snowBall.GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f);
+        demonLord.GetComponent<Image>().color = new Color(1f, 1f, 1f);
     }
 
     public void SkipTutorial()
