4adc4f044f844bd2736426f4ce3612d27f9526e0
amoamvr <143439516+amoam-vr@users.noreply.github.com>
2025-12-08 09:55:48 -0800

    applied knight and hunter sprites to hero library

--- Demon Lords Castle Defense/Assets/Scripts/Tools/Game/RhythmGameManager.cs
+++ Demon Lords Castle Defense/Assets/Scripts/Tools/Game/RhythmGameManager.cs
@@ -14,34 +14,45 @@ public class RhythmGameManager : MonoBehaviour
     // Contributors: Gustavo Rojas Flores
 
     // [need to change to private once debug done]
+    private int remainingNotes;
+
+    public GameObject[] heroNotes;
     public int totalScore;
-
-    private GameObject[] note;
-
     public RhythmGameType gameType;
+    public Transform[] heroSpawns;
 
-    private void Update()
-    {
-        // can get notes while spawning
-        note = GameObject.FindGameObjectsWithTag("Note");
-    }
+    //private void Update()
+    //{
+    //    // can get notes while spawning
+    //    note = GameObject.FindGameObjectsWithTag("Note");
+    //}
 
     public void StartMinigame(int noteCount)
     {
+        remainingNotes = noteCount;
 
+        NextNote();
     }
 
     private void NextNote()
     {
+        int lane = Random.Range(0, heroSpawns.Length);
 
+        GameObject newNote = Instantiate(heroNotes[Random.Range(0, heroNotes.Length)], heroSpawns[lane].position, Quaternion.identity);
+        //newNote.GetComponent<Note>
+    }
+
+    public void SuccessfulHit(int score)
+    {
+        totalScore += score;
     }
 
     public int UpdateScore()
     {
-        for (int i = 0; i < note.Length; i++)
-        {
-            totalScore += note[i].gameObject.GetComponent<RhythmicNote>().currentScore;
-        }
+        //for (int i = 0; i < note.Length; i++)
+        //{
+        //    totalScore += note[i].gameObject.GetComponent<RhythmicNote>().currentScore;
+        //}
 
         Debug.Log(totalScore);
         return totalScore;
